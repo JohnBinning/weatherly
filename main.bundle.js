081b9db9e9d9dd0eab79d86259effabaff2debb6
John Binning <john.binning17@gmail.com>
2017-07-06 19:42:53 -0600

    pics rel paths

--- main.bundle.js
+++ main.bundle.js
@@ -8073,7 +8073,7 @@
 
 	ReactDOM.render(React.createElement(_App2.default, null), document.getElementById('root'));
 
-	console.log('index');
+	console.log('index2');
 
 /***/ }),
 /* 298 */
@@ -23625,7 +23625,8 @@
 	      document.body.style.backgroundImage = _WeatherBackgrounds2.default[keys[i]];
 	      break;
 	    } else {
-	      document.body.style.backgroundImage = "url('../styles/pics/default_weather.jpeg')";
+	      document.body.style.backgroundImage = "url('./styles/pics/default_weather.jpeg')";
+	      // document.body.style.backgroundImage = "url('../styles/pics/default_weather.jpeg')";
 	    }
 	  }
 	};
@@ -23642,25 +23643,25 @@
 	  value: true
 	});
 	var WeatherBackgrounds = {
-	  nt_clear: "url('../styles/pics/clear_night.jpeg')",
-	  nt_sunny: "url('../styles/pics/clear_night.jpeg')",
-	  nt_partlysunny: "url('../styles/pics/clear_night.jpeg')",
-	  nt_mostlysunny: "url('../styles/pics/clear_night.jpeg')",
-	  nt_cloudy: "url('../styles/pics/cloudy_sunset.jpeg')",
-	  nt_mostlycloudy: "url('../styles/pics/cloudy_sunset.jpeg')",
-	  nt_partlycloudy: "url('../styles/pics/cloudy_sunset.jpeg')",
-	  nt_chancetstorms: "url('../styles/pics/big_lightning.jpeg')",
-	  nt_tstorms: "url('../styles/pics/big_lightning.jpeg')",
-	  flurries: "url('../styles/pics/flurry.jpeg')",
-	  snow: "url('../styles/pics/flurry.jpeg')",
-	  cloudy: "url('../styles/pics/clouds.jpeg')",
-	  rain: "url('../styles/pics/rain.jpeg')",
-	  tstorms: "url('../styles/pics/thick_lightning.jpeg')",
-	  sunny: "url('../styles/pics/Clear_Skies.jpeg')",
-	  clear: "url('../styles/pics/Clear_Skies.jpeg')",
-	  sleet: "url('../styles/pics/sleet.jpg')",
-	  fog: "url('../styles/pics/bw_fog.jpeg')",
-	  haze: "url('../styles/pics/haze.jpeg')"
+	  nt_clear: "url('./styles/pics/clear_night.jpeg')",
+	  nt_sunny: "url('./styles/pics/clear_night.jpeg')",
+	  nt_partlysunny: "url('./styles/pics/clear_night.jpeg')",
+	  nt_mostlysunny: "url('./styles/pics/clear_night.jpeg')",
+	  nt_cloudy: "url('./styles/pics/cloudy_sunset.jpeg')",
+	  nt_mostlycloudy: "url('./styles/pics/cloudy_sunset.jpeg')",
+	  nt_partlycloudy: "url('./styles/pics/cloudy_sunset.jpeg')",
+	  nt_chancetstorms: "url('./styles/pics/big_lightning.jpeg')",
+	  nt_tstorms: "url('./styles/pics/big_lightning.jpeg')",
+	  flurries: "url('./styles/pics/flurry.jpeg')",
+	  snow: "url('./styles/pics/flurry.jpeg')",
+	  cloudy: "url('./styles/pics/clouds.jpeg')",
+	  rain: "url('./styles/pics/rain.jpeg')",
+	  tstorms: "url('./styles/pics/thick_lightning.jpeg')",
+	  sunny: "url('./styles/pics/Clear_Skies.jpeg')",
+	  clear: "url('./styles/pics/Clear_Skies.jpeg')",
+	  sleet: "url('./styles/pics/sleet.jpg')",
+	  fog: "url('./styles/pics/bw_fog.jpeg')",
+	  haze: "url('./styles/pics/haze.jpeg')"
 	};
 
 	exports.default = WeatherBackgrounds;
